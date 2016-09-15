907f842ac4e9bfb8f60f2ea89a25bf64237c68bc
Isopach <kohyouliang@gmail.com>
2016-09-15 09:23:04 +0900

    Remove unused imports

--- rakuten.py
+++ rakuten.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 import random
